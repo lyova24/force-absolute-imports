46c5f5a49314f03771f0b8dafaf0240a4701a30c
lyova24 <lengthylyova@gmail.com>
2025-08-04 16:47:32 +0700

    fix: entrypoint

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import setup
 setup(
     name="force_absolute_imports",
     py_modules=["force_absolute_imports"],
+    entry_points={
+        "console_scripts": [
+            "force-absolute-imports = force_absolute_imports:main",
+        ],
+    },
 )
